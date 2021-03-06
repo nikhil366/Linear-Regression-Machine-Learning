df6c3d29f40865afd18d39f69fff6eb916406a11
nikhil366 <upadhyaynikhil1308@gmail.com>
2021-03-06 11:41:11 +0530

    few changes in Linear regression

--- venv/lib/python3.8/site-packages/pyforest/static/nbextension.js
+++
@@ -1,9 +0,0 @@
-define(['base/js/namespace', './utils'], function (Jupyter, utils) {
-	function load_ipython_extension() {
-		utils.setup_notebook(Jupyter);
-	}
-
-	return {
-		load_ipython_extension: load_ipython_extension
-	};
-});
